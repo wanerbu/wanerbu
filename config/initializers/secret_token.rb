93cd91eedacd6182591dec80e3cac5164cd536e1
dai <dai@dai-development.(none)>
2013-03-14 13:20:03 +0900

    init the wanerbu rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wanerbu::Application.config.secret_token = '92580f71f6edc8a0d55470632dcbba44434643e3ac68c71e35bd6ced4b0c68432bf2aaad72af5733c8c5d5e924c79649f00e0dc625964b2a0a4f6f86f9635cda'

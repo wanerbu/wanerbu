99bd64bf610fde86e1f85ee7f1481f80553a331b
MakeItEasy <rugang6891@gmail.com>
2013-04-06 21:02:19 +0900

    开会后的再提交一次

--- config/application.rb
+++ config/application.rb
@@ -55,6 +55,7 @@ module Wanerbu
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    #config.assets.paths += Dir[Rails.root.join('app', 'assets', 'stylesheets', '**/').to_s]
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'

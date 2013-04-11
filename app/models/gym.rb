13b0b89c050eb5826fec3d4cc9513e30d78d2a86
Tom Chen <chenniantao@gmail.com>
2013-04-11 10:56:03 +0800

    添加场馆电话的验证

--- app/models/gym.rb
+++ app/models/gym.rb
@@ -2,4 +2,6 @@ class Gym < ActiveRecord::Base
   # attr_accessible :title, :body
  attr_accessible :name,:intro,:address,:telephone,:open_time,:close_time,:score,:status,:deleted_at
 
+  validates :telephone, :length => { :in => 0..15 }
+  validates :telephone, :format => { if: "telephone.present?", with: Wanerbu::Common::FORMAT_TELEPHONE}
 end

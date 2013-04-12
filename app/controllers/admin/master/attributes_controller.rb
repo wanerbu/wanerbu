97a5edef9ae0e288bca0f49e569b847b39ef08a7
Tom Chen <chenniantao@gmail.com>
2013-04-12 19:19:12 +0800

    生成attributes的controller rails g controller admin/master/attributes

---
+++ app/controllers/admin/master/attributes_controller.rb
@@ -0,0 +1,2 @@
+class Admin::Master::AttributesController < ApplicationController
+end
---
+++ test/unit/helpers/admin/master/attributes_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class Admin::Master::AttributesHelperTest < ActionView::TestCase
+end

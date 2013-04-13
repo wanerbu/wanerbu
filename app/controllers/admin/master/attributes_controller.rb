018172b2c585ffe64b71e2f8ceb60e7c472711fe
Tom Chen <chenniantao@gmail.com>
2013-04-13 12:41:32 +0800

    1.attribute DB table生成 2.新建属性的实现

--- app/controllers/admin/master/attributes_controller.rb
+++ app/controllers/admin/master/attributes_controller.rb
@@ -6,5 +6,15 @@ class Admin::Master::AttributesController < Admin::AdminBaseController
 #
 #++
   def new
+    @attribute = Attribute.new
+  end
+  def create
+    @attribute = Attribute.new(params[:attribute])
+    if @attribute.save
+      redirect_to admin_master_attribute_path(@attribute), notice: I18n.t("activemodel.success.create", model: Attribute.model_name.human)
+    else
+      flash[:alert] = I18n.t("activemodel.errors.create", model: Attribute.model_name.human)
+      render :new
+    end
   end
 end
--- db/migrate/20130413031755_create_attributes.rb
+++ db/migrate/20130413031755_create_attributes.rb
@@ -1,8 +1,8 @@
 class CreateAttributes < ActiveRecord::Migration
   def change
     create_table :attributes do |t|
-      t.string :name, limit: 20,:null ==> false
-      t.string :unit, limit: 6,:null ==> false
+      t.string :name, limit: 20,:null => false
+      t.string :unit, limit: 6,:null => false
       t.string :intro, limit:50
 
       t.timestamps

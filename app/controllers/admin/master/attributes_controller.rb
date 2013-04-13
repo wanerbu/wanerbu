4d57f97e2693eded902350d263be5c830d7efa6b
Tom Chen <chenniantao@gmail.com>
2013-04-13 16:21:09 +0800

    属性删除逻辑的实现(暂时是物理删除)

--- app/controllers/admin/master/attributes_controller.rb
+++ app/controllers/admin/master/attributes_controller.rb
@@ -35,4 +35,15 @@ class Admin::Master::AttributesController < Admin::AdminBaseController
       render :edit
     end
   end
+  def destroy
+    @attribute = Attribute.find(params[:id])
+    # TODO Tom  存在性的check
+
+    if @attribute.destroy
+      redirect_to new_admin_master_attribute_path, notice: I18n.t("activemodel.success.destroy", model: Attribute.model_name.human)
+    else
+      flash[:alert] = I18n.t("activemodel.errors.destroy", model: Attribute.model_name.human)
+      render :show
+    end
+  end
 end

637c98dd44052ebef363b00084ed471d37b236c2
Tom Chen <chenniantao@gmail.com>
2013-04-14 15:59:54 +0800

    删除Attribute model rails d model Attribute

--- app/models/attribute.rb
+++
@@ -1,5 +0,0 @@
-class Attribute < ActiveRecord::Base
-  attr_accessible :intro, :name, :unit
-  has_many :sport_attributes, :dependent => :destroy
-  has_many :sports, :through => :sport_attributes
-end
--- test/unit/attribute_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class AttributeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end

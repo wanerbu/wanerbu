fb22770121d0d009f81612cbff9bcb289c69b92c
MakeItEasy <rugang6891@gmail.com>
2013-04-14 18:29:15 +0900

    每页显示数量的设定

--- app/controllers/admin/master/admin_reports_controller.rb
+++ app/controllers/admin/master/admin_reports_controller.rb
@@ -8,7 +8,7 @@
 class Admin::Master::AdminReportsController < Admin::AdminBaseController
   def index
     @admin_report = AdminReport.new(params[:admin_report])
-    @admin_report.assets.per_page = 1
+    @admin_report.assets.per_page = Wanerbu::Common::NUMBER_PER_PAGE
     @assets = @admin_report.assets.page(params[:page])
   end
 end
--- app/controllers/admin/master/role_reports_controller.rb
+++ app/controllers/admin/master/role_reports_controller.rb
@@ -8,7 +8,7 @@
 class Admin::Master::RoleReportsController < Admin::AdminBaseController
   def index
     @role_report = RoleReport.new(params[:role_report])
-    @role_report.assets.per_page = 1
+    @role_report.assets.per_page = Wanerbu::Common::NUMBER_PER_PAGE
     @assets = @role_report.assets.page(params[:page])
   end
 end

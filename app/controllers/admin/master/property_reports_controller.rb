b9825af51eda5029aebf1c57c0a8009255ee3ad4
Tom Chen <chenniantao@gmail.com>
2013-04-14 15:30:45 +0800

    Property一览功能实现

--- app/controllers/admin/master/property_reports_controller.rb
+++ app/controllers/admin/master/property_reports_controller.rb
@@ -1,2 +1,14 @@
-class Admin::Master::PropertyReportsController < ApplicationController
+# encoding: UTF-8
+#--
+# FileInfo:
+#   Author: Tom
+#
+#++
+#
+class Admin::Master::PropertyReportsController < Admin::AdminBaseController
+  def index
+    @property_report = PropertyReport.new(params[:property_report])
+    @property_report.assets.per_page = 10
+    @assets = @property_report.assets.page(params[:page])
+  end
 end
--- app/models/property_report.rb
+++ app/models/property_report.rb
@@ -1,3 +1,22 @@
-class PropertyReport < ActiveRecord::Base
-  # attr_accessible :title, :body
+# encoding: UTF-8
+#--
+# FileInfo:
+#   Author: Tom
+#
+#++
+# 属性Report
+#
+class PropertyReport
+  include Datagrid
+
+  scope do
+    Property
+  end
+
+  filter(:name, :string)
+
+  column(:name)
+  column(:unit)
+  column(:intro)
+  column(:actions, :html => true) { |asset| render :partial => "admin/master/property_reports/actions", :locals => {:target => asset} }
 end

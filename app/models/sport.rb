e013d48bab0960939529c282df84e18e7b894235
Tom Chen <chenniantao@gmail.com>
2013-04-14 10:42:28 +0800

    1.新加项目/属性/项目属性之间的association 2.在新建/编辑项目页面通过checkbox从DB中动态显示属性

--- app/models/sport.rb
+++ app/models/sport.rb
@@ -1,3 +1,5 @@
 class Sport < ActiveRecord::Base
-  attr_accessible :intro, :name, :sort
+  attr_accessible :intro, :name, :sort,:attribute_ids
+  has_many :sport_attributes, :dependent => :destroy
+  has_many :attributes, :through => :sport_attributes
 end
--- app/models/sport_attribute.rb
+++ app/models/sport_attribute.rb
@@ -1,3 +1,5 @@
 class SportAttribute < ActiveRecord::Base
   attr_accessible :attribute_id, :default_value, :required, :scope, :sport_id
+  belongs_to :sport
+  belongs_to :attribute
 end

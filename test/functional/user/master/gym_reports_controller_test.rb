0bd6343a856568b07be1606bc7eb75be05aeb8d6
Tom Chen <chenniantao@gmail.com>
2013-04-22 21:10:19 +0800

    在user master里删除场馆一览的controller rails d controller user/master/gym_reports

--- test/functional/user/master/gym_reports_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class User::Master::GymReportsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/unit/helpers/user/master/gym_reports_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class User::Master::GymReportsHelperTest < ActionView::TestCase
-end

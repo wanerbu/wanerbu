5f908da434ec7c352d84f0fa41dfb73c719728b9
MakeItEasy <rugang6891@gmail.com>
2013-04-23 14:30:57 +0900

    使普通用户可以使用用户id和email都可以登陆。注册的时候要求必须填写login id。

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,20 +15,48 @@ class User < ActiveRecord::Base
          #TODO Tom 暂时取消注册确认邮件功能 
          #:confirmable
   
+  # 逻辑删除
+  acts_as_paranoid
+
   # Virtual attribute for authenticating by either login_id or email
   attr_accessor :login 
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :password_salt,
-                  :login
+  attr_accessible :email,
+                  :password,
+                  :password_confirmation,
+                  :remember_me,
+                  :password_salt,
+                  :login,
+                  :login_id,
+                  :first_name,
+                  :last_name,
+                  :honey_name,
+                  :telephone_no,
+                  :sex,
+                  :birthday,
+                  :address,
+                  :status
+
   # attr_accessible :title, :body
+
+  ### validations
+  validates :login_id, :password_confirmation,  :presence => true, on: :create
+  validates :login_id, :length => { :in => 6..16 }, if: "login_id.present?", on: :create
+  validates :login_id, :format => { if: "login_id.present?", with: Wanerbu::Common::FORMAT_LOGIN_ID }, on: :create
+  validates :first_name, :last_name, :length => { :in => 0..10 }
+  validates :telephone_no, :length => { :in => 0..15 }
+  validates :telephone_no, :format => { if: "telephone_no.present?", with: Wanerbu::Common::FORMAT_TELEPHONE}
 
   #添加association
   has_one :gym
 
-  before_save do
-    # TODO dairg 临时的处理
-    self.login_id = self.email
+  # 姓名
+  def name
+    name = ''
+    name += self.first_name + ' ' if self.first_name
+    name += self.last_name if self.last_name
+    return name
   end
 
   # 覆盖devise的这个方法，可以修改认证登录的条件

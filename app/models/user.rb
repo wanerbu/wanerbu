87b5374f5c7e2055fe93e6df341ec2508441728a
MakeItEasy <rugang6891@gmail.com>
2013-04-23 13:57:04 +0900

    允许普通用户使用login_id进行登陆

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,11 +11,16 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :token_authenticatable, :encryptable,:lockable, :timeoutable
+         :token_authenticatable, :encryptable,:lockable, :timeoutable, :authentication_keys => [:login]
          #TODO Tom 暂时取消注册确认邮件功能 
          #:confirmable
+  
+  # Virtual attribute for authenticating by either login_id or email
+  attr_accessor :login 
+
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :password_salt
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :password_salt,
+                  :login
   # attr_accessible :title, :body
 
   #添加association
@@ -25,4 +30,16 @@ class User < ActiveRecord::Base
     # TODO dairg 临时的处理
     self.login_id = self.email
   end
+
+  # 覆盖devise的这个方法，可以修改认证登录的条件
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      # TODO dairg 大小写问题的确认
+      where(conditions).where(["lower(login_id) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
+
 end

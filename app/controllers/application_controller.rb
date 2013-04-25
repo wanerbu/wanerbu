3345ea8881d36d7d26c531001c1a52ae47659ed9
MakeItEasy <rugang6891@gmail.com>
2013-04-25 18:39:50 +0900

    修改了前后台用户，点击退出后的跳转页面

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+private 
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(resource_or_scope)
+    # 如果是后台用户退出，那么回到登录画面，如果是前台用户退出，回到首页
+    resource_or_scope == :admin ? new_admin_session_path : root_path
+  end
 end

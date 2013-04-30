e8ec6963e2efa6c652ae99d9a1d3babd082834fa
MakeItEasy <rugang6891@gmail.com>
2013-04-30 18:43:11 +0900

    表头部分样式调整完毕，还有就是datagrid的表头显示修改完毕

--- app/models/admin_report.rb
+++ app/models/admin_report.rb
@@ -19,7 +19,11 @@ class AdminReport
   column(:login_id)
   column(:email)
   column(:name)
-  column(:created_at)
-  column(:actions, :html => true) { |asset| render :partial => "admin/master/admin_reports/actions", :locals => {:target => asset} }
+  column(:created_at, header: I18n.t('views.defaults.label.created_at')) do
+    I18n.l(self.created_at.to_date)
+  end
+  column(:actions, header: I18n.t('views.defaults.label.actions'), :html => true) do |asset|
+    render :partial => "admin/master/admin_reports/actions", :locals => {:target => asset}
+  end
 end
 

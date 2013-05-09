2f7fdfc1bf9cdf6a6ac947f457803403e62290a5
Tom Chen <chenniantao@gmail.com>
2013-05-09 21:52:01 +0800

    新建属性的验证

--- app/models/property.rb
+++ app/models/property.rb
@@ -3,4 +3,8 @@ class Property < ActiveRecord::Base
   attr_accessible :intro, :name, :unit
   has_many :sport_properties, :dependent => :destroy
   has_many :sports, :through => :sport_properties
+  # validations
+  validates :name, 
+    :presence => true,
+    :length => { :in => 1..20 }
 end

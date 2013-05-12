31e4211ecaad55cc5e775fe7011ade7bf7063778
Tom Chen <chenniantao@gmail.com>
2013-05-12 15:06:56 +0800

    场次一览模糊查询by项

--- app/models/game_report.rb
+++ app/models/game_report.rb
@@ -10,16 +10,22 @@ class GameReport
   include Datagrid
 
   scope do
-    Game
+    Game.order("games.court_id asc")
   end
-#TODO 这里需要按项目名查找
-  filter(:name, :string)
+
+  #TODO select的名字显示需要调查，暂时用下面的模糊查询
+  #filter(:court_id, :enum, :select => ["980190968", "2"],:header => "项目") 
+
+  filter(:court_id, :string,:header => "项目") do |value|
+    self.where(["court_id = ?", court_id = (@court = Court.where(:sport_id => sport_id = (@sport = Sport.where("name LIKE '%#{value}%' ").first) ? @sport.id : "").first) ? @court.id : ""])
+  end
 
   column("项目") do
     Sport.find(Court.find(self.court_id).sport_id).name
   end
   column(:name)
   column(:default_price)
+  column(:sort)
   column(:actions, header: I18n.t('views.defaults.label.actions'), :html => true) do |asset|
     render :partial => "user/master/game_reports/actions", :locals => {:target => asset}
   end

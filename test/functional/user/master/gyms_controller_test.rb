8eef3e1dbbf1de762ffe7abe2c02f7fed0f124bb
Tom Chen <chenniantao@gmail.com>
2013-05-13 23:36:54 +0800

    添加场馆Functional Tests case

--- test/functional/user/master/gyms_controller_test.rb
+++ test/functional/user/master/gyms_controller_test.rb
@@ -8,4 +8,41 @@ class User::Master::GymsControllerTest < ActionController::TestCase
     should_redirect_to_user_login
   end
 
+  test "should redirect to login when get edit" do
+    get :edit, {'id' => "2"}
+    should_redirect_to_user_login
+  end
+
+  ### 已经登录的情况
+  test "should get new when user no gym" do
+    sign_in :user, users(:user_unconfirmed_no_gym)
+
+    get :new
+    assert_response :success
+
+    sign_out :user
+  end
+  test "should not get new when user have gym" do
+    sign_in :user, users(:user_unconfirmed)
+
+    get :new
+    assert_response :redirect
+
+    sign_out :user
+  end
+  test "should get edit owned gym " do
+    sign_in :user, users(:user_unconfirmed)
+    get :edit, {'id' => "2"}
+    assert_response :success
+
+    sign_out :user
+  end
+  test "should not get edit if not owned gym " do
+    sign_in :user, users(:user_unconfirmed)
+
+    get :edit, {'id' => "1"}
+    assert_response 403
+
+    sign_out :user
+  end
 end

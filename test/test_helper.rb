803c568593f3683421e94f0137fedf410ac58b26
MakeItEasy <rugang6891@gmail.com>
2013-05-13 18:56:31 +0900

    添加了通用的两个测试断言方法，主要是判断重定向到login画面的

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,4 +10,20 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  
+  # 用来判断是否迁移到了admin的登录画面
+  def should_redirect_to_admin_login
+    assert_redirected_to new_admin_session_url
+  end
+
+  # 用来判断是否迁移到了user的登录画面
+  def should_redirect_to_user_login
+    assert_redirected_to new_user_session_url
+  end
 end
+
+# 添加了Devise的一些用于测试的helper
+class ActionController::TestCase
+  include Devise::TestHelpers
+end
+

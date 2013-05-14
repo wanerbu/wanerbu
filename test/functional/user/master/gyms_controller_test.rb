c35c0b5961e72e2093db12fafd7a67811f0499ec
Tom Chen <chenniantao@gmail.com>
2013-05-14 22:59:59 +0800

    添加gyms controller test用例

--- test/functional/user/master/gyms_controller_test.rb
+++ test/functional/user/master/gyms_controller_test.rb
@@ -14,6 +14,16 @@ class User::Master::GymsControllerTest < ActionController::TestCase
   end
 
   ### 已经登录的情况
+  test "should not get index if user no gym" do
+    sign_in :user, users(:user_unconfirmed_no_gym)
+    get :index
+    assert_response 403
+  end
+  test "should get index if user have gym" do
+    sign_in :user, users(:user_unconfirmed)
+    get :index
+    assert_response :success
+  end
   test "should get new when user no gym" do
     sign_in :user, users(:user_unconfirmed_no_gym)
 
@@ -45,4 +55,35 @@ class User::Master::GymsControllerTest < ActionController::TestCase
 
     sign_out :user
   end
+  test "should not get edit owned bym but not gymer " do
+    sign_in :user, users(:user_not_gymer)
+
+    get :edit, {'id' => "4"}
+    assert_response 403
+
+    sign_out :user
+  end
+  test "should create gym successfully when user no gym" do
+    sign_in :user, users(:user_unconfirmed_no_gym)
+    tmp_gym = { 
+      name: "tmp gym",
+      intro: "tmp gym intro",
+      address: "tmp gym address",
+      telephone: 222222222,
+      open_time: Time.now,
+      close_time: Time.now,
+      user_id:  3
+    }
+    assert_difference("Gym.count", 1) do
+      post :create, :gym => tmp_gym
+    end
+    created_gym = assigns(:gym)
+    assert_not_nil created_gym
+    assert_not_nil created_gym.id
+    assert_equal tmp_gym[:name], created_gym.name
+    assert_equal tmp_gym[:intro], created_gym.intro
+    assert_equal I18n.t("activemodel.success.create", model: Gym.model_name.human), flash[:notice]
+    assert_redirected_to user_master_gym_path(assigns[:gym])
+  end
+
 end

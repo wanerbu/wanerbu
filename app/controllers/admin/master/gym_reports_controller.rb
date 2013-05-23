7583fda6d272445ce548b8a76e070c2709b38ede
Tom Chen <chenniantao@gmail.com>
2013-05-23 22:40:38 +0800

    暂时取消GYM report的权限检查

--- app/controllers/admin/master/gym_reports_controller.rb
+++ app/controllers/admin/master/gym_reports_controller.rb
@@ -7,8 +7,6 @@
 #
 class Admin::Master::GymReportsController < Admin::AdminBaseController
 
-  # 对于每一个action进行权限检查
-  load_and_authorize_resource 
   def index
     @gym_report = GymReport.new(params[:gym_report])
     @gym_report.assets.per_page =  Wanerbu::Common::NUMBER_PER_PAGE

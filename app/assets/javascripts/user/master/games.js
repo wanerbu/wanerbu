d4d68a37499e4950520ef08f6fb0060627ab75fe
Tom Chen <chenniantao@gmail.com>
2013-07-16 22:50:19 +0800

    规则类型选择互斥的javascript实现

--- app/assets/javascripts/user/master/games.js
+++ app/assets/javascripts/user/master/games.js
@@ -1,9 +1,25 @@
 //# Place all the behaviors and hooks related to the matching controller here.
 //# All this logic will automatically be available in application.js.
 //# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
-document.getElementById('price_rule_rule_type').onchange = function () {
-  var date_style = this.value == '00' ? 'block' : 'none';
-  var week_style = this.value == '01' ? 'block' : 'none';
-  document.getElementById('select_date_price_rule').style.display = date_style;
-  document.getElementById('select_week_price_rule').style.display = week_style;
-};
+$(document).ready(function(){
+    $("#price_rule_rule_type").change(function() {
+      var price_rule_type = $('select#price_rule_rule_type :selected').val();
+      if (price_rule_type == '01'){
+         $('#select_date_price_rule').show();    
+         $('#select_week_price_rule').hide();    
+       }else{
+         $('#select_date_price_rule').hide();    
+         $('#select_week_price_rule').show();    
+       }
+    });  
+    $("#reservation_rule_rule_type").change(function() {
+      var reservation_rule_type = $('select#reservation_rule_rule_type :selected').val();
+      if (reservation_rule_type == '01'){
+         $('#select_date_reservation_rule').show();    
+         $('#select_week_reservation_rule').hide();    
+       }else{
+         $('#select_date_reservation_rule').hide();    
+         $('#select_week_reservation_rule').show();    
+       }
+    });  
+});

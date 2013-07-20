f461ad81ab529b7558f13a2b629a91293cedf83b
Tom Chen <chenniantao@gmail.com>
2013-07-20 22:10:58 +0800

    省市区联动功能完善

--- app/assets/javascripts/user/master/cities.js
+++ app/assets/javascripts/user/master/cities.js
@@ -2,7 +2,17 @@
 //# All this logic will automatically be available in application.js.
 //# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
 $(document).ready(function(){
-    $("#gym_city_id").change(function() {
+  //页面刷新时 
+      var city_id = $('select#gym_city_id :selected').val();
+
+      if(city_id == "") city_id ="0";
+
+      jQuery.get('get_areas_by_city_id/' + city_id,
+          function(data){
+              $("#areas").html(data);
+                });
+  //用户选择时
+  $("#gym_city_id").change(function() {
       var city_id = $('select#gym_city_id :selected').val();
 
       if(city_id == "") city_id ="0";
--- app/assets/javascripts/user/master/gyms.js
+++ app/assets/javascripts/user/master/gyms.js
@@ -2,9 +2,17 @@
 //# All this logic will automatically be available in application.js.
 //# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
 $(document).ready(function(){
+  //页面刷新时根据省ID取城市 
+  var province_id = $('select#gym_province_id :selected').val();
+   if(province_id == "") province_id ="0";
+
+   jQuery.get('get_cities_by_province_id/' + province_id,
+              function(data){
+                   $("#cities").html(data);
+                });
+  //选择某个省时根据省ID取城市
     $("#gym_province_id").change(function() {
       var province_id = $('select#gym_province_id :selected').val();
-
       if(province_id == "") province_id ="0";
 
       jQuery.get('get_cities_by_province_id/' + province_id,

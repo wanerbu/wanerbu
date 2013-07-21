b172e8b1497969269fabd2975125be86d71e6220
Tom Chen <chenniantao@gmail.com>
2013-07-21 17:19:18 +0800

    新建场地项目属性在页面加载时初始化

--- app/assets/javascripts/user/master/courts.js
+++ app/assets/javascripts/user/master/courts.js
@@ -1,8 +1,16 @@
 //# Place all the behaviors and hooks related to the matching controller here.
 //# All this logic will automatically be available in application.js.
 //# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
-//#TODO 页面加载时初始化未实现
 jQuery(function($) {
+  //页面刷新时 
+    var sport = $('select#court_sport_id :selected').val();
+    if(sport == "") sport="0";
+    jQuery.get('edit_court_properties_select/' + sport,
+    function(data){
+     $("#courtProperties").html(data);
+                  });
+
+  //用户选择时
   $("#court_sport_id").change(function() {
     var sport = $('select#court_sport_id :selected').val();
     if(sport == "") sport="0";

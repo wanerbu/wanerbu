6b2d27cfc6b3e825abd15eb4d33d4cae1dc8bfc3
Tom Chen <chenniantao@gmail.com>
2013-11-23 21:42:30 +0800

    完善查询条件

--- app/controllers/gym_controller.rb
+++ app/controllers/gym_controller.rb
@@ -3,7 +3,19 @@ class GymController < ApplicationController
     @gyms = Gym.paginate(:page => params[:page],:per_page => 1).order('id asc')
   end
   def search
-    @gyms = Gym.where(:area_id => params[:areaid]).paginate(:page => params[:page],:per_page => 1).order('id asc')
+   $sql = "1 = 1 "
+   if params[:areaid] != "all"then
+    $sql = $sql + " and area_id = " + params[:areaid]
+   end  
+   if params[:sportid] != "all"then
+    $sql = $sql + " and id in ("
+    courts = Court.where(:sport_id => params[:sportid])
+    courts.each do |court|
+      $sql = $sql + court.gym_id.to_s + ","
+    end
+    $sql = $sql + "0)"
+   end  
+    @gyms = Gym.where($sql).paginate(:page => params[:page],:per_page => 1).order('id asc')
     render :partial => "gymlist"
   end
 end
